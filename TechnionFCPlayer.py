04cafe91d801658757abc62f813816702b05155c
Sadoun, Sean <seaninho10@gmail.com>
2021-12-25 22:31:20 +0200

    Add bot 'approve' user command

--- TechnionFCPlayer.py
+++ TechnionFCPlayer.py
@@ -1,8 +1,9 @@
 class TechnionFCPlayer:
     """This object represents a Technion FC player"""
-    def __init__(self, user, liable=False):
-        self.user = user            # telegram User object
-        self.liable = liable        # match liability
+    def __init__(self, user, liable=False, approved=False):
+        self.user = user                # telegram User object
+        self.liable = liable            # match liability
+        self.approved = approved        # indicates whether the player has approved he'll be attending
 
     def __eq__(self, other):
         if isinstance(other, TechnionFCPlayer):
