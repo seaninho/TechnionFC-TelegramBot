66561e52a26c21167e155e009a2b5f470d9d1df3
Sadoun, Sean <seaninho10@gmail.com>
2021-12-25 23:40:11 +0200

    Add bot 'ball' user command

--- TechnionFCPlayer.py
+++ TechnionFCPlayer.py
@@ -1,9 +1,10 @@
 class TechnionFCPlayer:
     """This object represents a Technion FC player"""
-    def __init__(self, user, liable=False, approved=False):
-        self.user = user                # telegram User object
-        self.liable = liable            # match liability
-        self.approved = approved        # indicates whether the player has approved he'll be attending
+    def __init__(self, user, liable=False, approved=False, match_ball=False):
+        self.user = user                    # telegram User object
+        self.liable = liable                # match liability
+        self.approved = approved            # indicates whether the player has approved he'll be attending
+        self.match_ball = match_ball        # indicates whether the player will bring a match ball
 
     def __eq__(self, other):
         if isinstance(other, TechnionFCPlayer):
