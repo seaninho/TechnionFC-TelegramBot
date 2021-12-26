dfbd7429994a938d3466ca496e0c91432cab382f
Sadoun, Sean <seaninho10@gmail.com>
2021-12-26 06:38:31 +0200

    Add 'backup_to_database' job to perform list back up

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
+# Telegram bot
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '')
 TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', '')
 TELEGRAM_GROUP_INVITE_LINK = os.environ.get('TELEGRAM_GROUP_INVITE_LINK', '')
 PORT = int(os.environ.get('PORT', 8443))
+
+# Postgres connection
+DATABASE_URL = os.environ.get('DATABASE_URL', '')
