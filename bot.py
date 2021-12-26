ff512a087f42996d00106e6ef873fb59ad5b0208
Sadoun, Sean <seaninho10@gmail.com>
2021-12-26 02:31:22 +0200

    Add bot 'clearAll' admin command

--- bot.py
+++ bot.py
@@ -49,6 +49,18 @@ def start_command(update, context):
 
     user.send_message(message, parse_mode='MarkdownV2')
 
+
+def clearAll_command(update, context):
+    """Clear both playing and waiting lists"""
+    user = update.message.from_user
+    if str(update.message.chat.id) != TELEGRAM_CHAT_ID:
+        return update.message.reply_text(get_command_in_private_warning(user, 'clearAll'))
+    if is_group_admin(update, context, user):
+        playing.clear()
+        invited.clear()
+        return update.message.reply_text('Both lists were cleared by an admin')
+    update.message.reply_text(f'Hi {user.full_name}!\n\nPlease note, only admins are allowed to clear the lists!')
+
 # endregion
 
 # region USER COMMANDS
@@ -96,7 +108,8 @@ def help_command(update, context):
               f'/rules \- print match rules\n' \
               f'/schedule \- print the bot\'s schedule\n' \
               f'\n*Available only to admins* :\n' \
-              f'/start \- start the bot\n'
+              f'/start \- start the bot\n' \
+              f'/clearAll \- clear the list\n'
 
     user.send_message(message, parse_mode='MarkdownV2')
 
@@ -522,6 +535,7 @@ def main():
     dp.add_handler(CommandHandler("print", print_command))
     dp.add_handler(CommandHandler("rules", rules_command))
     dp.add_handler(CommandHandler("schedule", schedule_command))
+    dp.add_handler(CommandHandler("clearAll", clearAll_command))
 
     # log all errors
     dp.add_error_handler(error)
