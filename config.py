f23eb53ec68fe54cede24e5a8921c0fe911b2948
Sadoun, Sean <seaninho10@gmail.com>
2021-12-26 06:38:31 +0200

    Add 'backup_to_database' job to perform list back up

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+# Telegram bot
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '')
 TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', '')
 PORT = int(os.environ.get('PORT', 8443))
+
+# Postgres connection
+DATABASE_URL = os.environ.get('DATABASE_URL', '')
---
+++ postgres.py
@@ -0,0 +1,27 @@
+import os
+import psycopg2
+
+from config import DATABASE_URL
+
+if os.environ.get("HEROKU"):
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+else:
+    conn = psycopg2.connect(DATABASE_URL)
+
+cur = conn.cursor()
+
+# cur.execute("DROP TABLE playing")
+cur.execute("CREATE TABLE IF NOT EXISTS PLAYING ("
+            "   user_id BIGINT,"
+            "   user_first_name VARCHAR,"
+            "   user_last_name VARCHAR,"
+            "   user_username VARCHAR,"
+            "   player_liable BOOLEAN NOT NULL,"
+            "   player_approved BOOLEAN NOT NULL,"
+            "   player_match_ball BOOLEAN NOT NULL,"
+            "   PRIMARY KEY (user_id, user_first_name, user_last_name, user_username))")
+
+cur.execute("CREATE TABLE IF NOT EXISTS INVITED ("            
+            "   username VARCHAR PRIMARY KEY)")
+cur.close()
+conn.commit()
