f6bf28852a9adeb06a383bafd3b05eab804eacf5
Sadoun, Sean <seaninho10@gmail.com>
2022-08-05 18:45:35 +0300

    Add PostgreSQL database error handling
    Also added PostgreSQL class that holds all relevant SQL database functions

--- postgres.py
+++ postgres.py
@@ -3,28 +3,46 @@ import psycopg2
 
 from config import DATABASE_URL
 
-if os.environ.get("HEROKU"):
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-else:
-    conn = psycopg2.connect(DATABASE_URL)
 
-cur = conn.cursor()
+class PostgreSqlDb:
+    """This class holds all relevant SQL database functions"""
+    def __init__(self):
+        self.init_connection()
 
-# cur.execute("DROP TABLE playing")
-cur.execute("CREATE TABLE IF NOT EXISTS PLAYING ("
-            "   user_id BIGINT,"
-            "   user_first_name VARCHAR,"
-            "   user_last_name VARCHAR,"
-            "   user_username VARCHAR,"
-            "   player_liable BOOLEAN NOT NULL,"
-            "   player_approved BOOLEAN NOT NULL,"
-            "   player_match_ball BOOLEAN NOT NULL,"
-            "   PRIMARY KEY (user_id, user_first_name, user_last_name, user_username))")
+    def init_connection(self):
+        """Initialize a new connection to the PostgreSQL database"""
+        self._connect()
+        self._create_tables()
 
-cur.execute("CREATE TABLE IF NOT EXISTS INVITED ("            
-            "   username VARCHAR PRIMARY KEY)")
+    def get_connection(self):
+        """Get the PostgreSQL database connection"""
+        return self._connection
 
-cur.execute("CREATE TABLE IF NOT EXISTS ASKED ("            
-            "   user_id_or_name VARCHAR PRIMARY KEY)")
-cur.close()
-conn.commit()
+    def _connect(self):
+        """Create a connection to the PostgreSQL database"""
+        if os.environ.get("HEROKU"):
+            self._connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+        self._connection = psycopg2.connect(DATABASE_URL)
+
+    def _create_tables(self):
+        """Create database tables"""
+        cur = self._connection.cursor()
+
+        # cur.execute("DROP TABLE playing")
+        cur.execute("CREATE TABLE IF NOT EXISTS PLAYING ("
+                    "   user_id BIGINT,"
+                    "   user_first_name VARCHAR,"
+                    "   user_last_name VARCHAR,"
+                    "   user_username VARCHAR,"
+                    "   player_liable BOOLEAN NOT NULL,"
+                    "   player_approved BOOLEAN NOT NULL,"
+                    "   player_match_ball BOOLEAN NOT NULL,"
+                    "   PRIMARY KEY (user_id, user_first_name, user_last_name, user_username))")
+
+        cur.execute("CREATE TABLE IF NOT EXISTS INVITED ("            
+                    "   username VARCHAR PRIMARY KEY)")
+
+        cur.execute("CREATE TABLE IF NOT EXISTS ASKED ("            
+                    "   user_id_or_name VARCHAR PRIMARY KEY)")
+        cur.close()
+        self._connection.commit()
